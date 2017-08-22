e5d568a5c82755aefe5cb367db24762cf341d20f
Dave Gumba <dave_gumba@Yahoo.com>
2017-08-22 02:20:56 -0400

    last two done gbut hardcoded

--- rsfishing.py
+++ rsfishing.py
@@ -9,17 +9,9 @@ def clear_inventory(inventory_icon, first_ore, cooked):
   ore_dy = 34 # change in y in inventory
   drop_factor = 53 if cooked else 36
   x_pos_inv = first_ore[0]
-  i = 0
-  the_range = 6
   for j in range(4):
     y_pos_inv = first_ore[1]
-    if i == 2:
-        the_range = 8
-    i += 1
-    for k in range(the_range):
-
-      if the_range > 5:
-        drop_factor = 30
+    for k in range(6):
 
 	  # right click
       pyautogui.rightClick(x=x_pos_inv, y=y_pos_inv)
@@ -33,8 +25,27 @@ def clear_inventory(inventory_icon, first_ore, cooked):
       y_pos_inv += ore_dy
     x_pos_inv += ore_dx
 
+def last_two(pos):
+    ore_dx = 42
+    ore_dy = 34
+
+    pyautogui.rightClick(pos[0], pos[1])
+
+    pyautogui.moveTo(x=pos[0], y=pos[1]+20)
+
+    pyautogui.click()
+
+    pyautogui.moveTo(x=pos[0], y=pos[1])
+
+    pyautogui.rightClick(pos[0] + 42, y=pos[1])
+
+    pyautogui.moveTo(x=pos[0] + 42, y=pos[1] + 20)
+
+    pyautogui.click()
 
 first_fish_in_inv = (1249, 301) # this is the coordinate of first fish in inventory
+last_two_pos = (1334, 518)
 inventory_icon = (1318, 259)
 
 clear_inventory(inventory_icon, first_fish_in_inv, False)
+last_two(last_two_pos)
