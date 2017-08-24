96fc749046c003f993a329196113c97e789b78a6
Dave Gumba <dave_gumba@Yahoo.com>
2017-08-24 08:12:48 -0400

    modified rsmining

--- rsmining.py
+++ rsmining.py
@@ -6,13 +6,13 @@ import time
 def clear_inventory(inventory_icon, first_ore):
   pyautogui.click(x=inventory_icon[0], y=inventory_icon[1])
   ore_dx = 42 # change in x in inventory
-  ore_dy = 34 # change in y in inventory 
+  ore_dy = 34 # change in y in inventory
   drop_factor = 36
   x_pos_inv = first_ore[0]
   for j in range(4):
     y_pos_inv = first_ore[1]
     for k in range(6):
-	  # right click 
+	  # right click
       pyautogui.rightClick(x=x_pos_inv, y=y_pos_inv)
 	  # move mouse to drop option
       pyautogui.moveTo(x=x_pos_inv, y=y_pos_inv+drop_factor)
@@ -35,23 +35,59 @@ def three_ores(time_limit, ores):
     pyautogui.click(x=ores[1][0], y=ores[1][1], clicks=1, interval=7, button='left')
     pyautogui.click(x=ores[2][0], y=ores[2][1], clicks=1, interval=11, button='left')
 
+def two_ores(time_limit, ore1, ore2):
+    while time.time() < time_limit:
+        pyautogui.click(x=ore1[0], y=ore1[1], clicks=1, interval=3, button='left')
+        time.sleep(2)
+        pyautogui.click(x=ore2[0], y=ore2[1], clicks=1, interval=7, button='left')
+
+def last_two(pos):
+    ore_dx = 42
+    ore_dy = 34
+
+    pyautogui.rightClick(pos[0], pos[1])
+
+    pyautogui.moveTo(x=pos[0], y=pos[1]+20)
+
+    pyautogui.click()
+
+    pyautogui.moveTo(x=pos[0], y=pos[1])
+
+    pyautogui.rightClick(pos[0] + 42, y=pos[1])
+
+    pyautogui.moveTo(x=pos[0] + 42, y=pos[1] + 20)
+
+    pyautogui.click()
+
+    pyautogui.rightClick(pos[0] + 84, y=pos[1])
+
+    pyautogui.moveTo(x=pos[0] + 84, y=pos[1] + 20)
+
+    pyautogui.click()
+
 # VARIABLES THAT CHANGE
-ore_location = (918, 366) # this is the coordinate of the ore i'm clicking on
-ores = ((899, 222), (898, 331), (1064, 346))
-first_ore_in_inv = (1254, 301) # this is the coordinate of first ore in inventory
+ore_location = (829, 196) # this is the coordinate of the ore i'm clicking on
+ores = ((871, 183), (990, 223))
+first_ore_in_inv = (1254, 303) # this is the coordinate of first ore in inventory
 inventory_icon = (1318, 259)
-minutes_mining = 8 # this might change often
+minutes_mining = 4 # this might change often
+last_two_pos = (1295, 520)
 
 # CONSTANTS
 RUNNING = True
 DURATION_MINING = time.time() + 60 * minutes_mining # just more readable
 
-#while RUNNING:
 
-  #one_ore(DURATION_MINING, ore_location)
-  
-  #clear_inventory(inventory_icon, first_ore_in_inv)
-  #DURATION_MINING = time.time() + 60 * minutes_mining # just more readable
-  #time.sleep(3)
+while RUNNING:
+
+  two_ores(DURATION_MINING, ores[0], ores[1])
+
+  clear_inventory(inventory_icon, first_ore_in_inv)
+  last_two(last_two_pos)
+  DURATION_MINING = time.time() + 60 * minutes_mining # just more readable
+  time.sleep(2)
+
+
 
 clear_inventory(inventory_icon, first_ore_in_inv)
+last_two(last_two_pos)
